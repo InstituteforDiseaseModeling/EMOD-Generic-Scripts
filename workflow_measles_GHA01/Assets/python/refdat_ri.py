c7968a8fa9f26485be1e61cf75c8a713e0afe96a
Kurt Frey <kfrey@idmod.org>
2022-01-08 15:35:19 -0800

    Spatial variation in RI

--- workflow_measles_GHA01/Assets/python/refdat_ri.py
+++ workflow_measles_GHA01/Assets/python/refdat_ri.py
@@ -6,10 +6,100 @@
 
 data_dict = \
 {
-   "GHA": {
-      "cover":  0.85,
+   "GHA_RI_DHS_2014_00001": {
+      "cover": 0.856,
       "nodes": [
-         "AFRO:GHANA"
+         "AFRO:GHANA:WESTERN"
+      ]
+   },
+   "GHA_RI_DHS_2014_00002": {
+      "cover": 0.856,
+      "nodes": [
+         "AFRO:GHANA:WESTERN_NORTH"
+      ]
+   },
+   "GHA_RI_DHS_2014_00003": {
+      "cover": 0.902,
+      "nodes": [
+         "AFRO:GHANA:CENTRAL"
+      ]
+   },
+   "GHA_RI_DHS_2014_00004": {
+      "cover": 0.922,
+      "nodes": [
+         "AFRO:GHANA:GREATER_ACCRA"
+      ]
+   },
+   "GHA_RI_DHS_2014_00005": {
+      "cover": 0.838,
+      "nodes": [
+         "AFRO:GHANA:VOLTA"
+      ]
+   },
+   "GHA_RI_DHS_2014_00006": {
+      "cover": 0.838,
+      "nodes": [
+         "AFRO:GHANA:OTI"
+      ]
+   },
+   "GHA_RI_DHS_2014_00007": {
+      "cover": 0.869,
+      "nodes": [
+         "AFRO:GHANA:EASTERN"
+      ]
+   },
+   "GHA_RI_DHS_2014_00008": {
+      "cover": 0.951,
+      "nodes": [
+         "AFRO:GHANA:ASHANTI"
+      ]
+   },
+   "GHA_RI_DHS_2014_00009": {
+      "cover": 0.93,
+      "nodes": [
+         "AFRO:GHANA:AHAFO"
+      ]
+   },
+   "GHA_RI_DHS_2014_00010": {
+      "cover": 0.93,
+      "nodes": [
+         "AFRO:GHANA:BONO"
+      ]
+   },
+   "GHA_RI_DHS_2014_00011": {
+      "cover": 0.93,
+      "nodes": [
+         "AFRO:GHANA:BONO_EAST"
+      ]
+   },
+   "GHA_RI_DHS_2014_00012": {
+      "cover": 0.794,
+      "nodes": [
+         "AFRO:GHANA:NORTHERN_EAST"
+      ]
+   },
+   "GHA_RI_DHS_2014_00013": {
+      "cover": 0.794,
+      "nodes": [
+         "AFRO:GHANA:NORTHERN"
+      ]
+   },
+   "GHA_RI_DHS_2014_00014": {
+      "cover": 0.794,
+      "nodes": [
+         "AFRO:GHANA:SAVANNAH"
+      ]
+   },
+   "GHA_RI_DHS_2014_00015": {
+      "cover": 0.964,
+      "nodes": [
+         "AFRO:GHANA:UPPER_WEST"
+      ]
+   },
+   "GHA_RI_DHS_2014_00016": {
+      "cover": 0.921,
+      "nodes": [
+         "AFRO:GHANA:UPPER_EAST"
       ]
    }
 }
