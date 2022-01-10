868d210dd8f0e93788b7d5565f4fc42286a62a6b
Kurt Frey <kfrey@idmod.org>
2022-01-09 19:20:29 -0800

    Seasonality and variable SIA coverages

--- workflow_measles_GHA01/Assets/python/builder_campaign.py
+++ workflow_measles_GHA01/Assets/python/builder_campaign.py
@@ -29,19 +29,17 @@ def campaignBuilder():
 
   # ***** Get variables for this simulation *****
   TIME_START   = gdata.var_params['start_time']
-  SIA_COVER    = gdata.var_params['SIA_coverage']
-
+  PEAK_SIZE    = gdata.var_params['R0_peak_magnitude']
+  PEAK_TIME    = gdata.var_params['R0_peak_day']
 
   # ***** Events *****
 
   node_dict = gdata.demog_node
   node_opts = list(node_dict.keys())
 
-
   # Add MCV RI
   for ri_name in dict_ri:
     ri_obj    = dict_ri[ri_name]
-    start_val  = TIME_START
     coverage   = ri_obj['cover']
     node_list  = list()
     for targ_val in ri_obj['nodes']:
@@ -49,7 +47,7 @@ def campaignBuilder():
         if((node_name == targ_val) or (node_name.startswith(targ_val+':'))):
           node_list.append(node_dict[node_name])
 
-    pdict     = {'startday':       start_val ,
+    pdict     = {'startday':       TIME_START ,
                  'nodes':          node_list ,
                  'coverage':       coverage  }
 
@@ -59,6 +57,7 @@ def campaignBuilder():
   # Add MCV SIAs
   for sia_name in dict_sia:
     sia_obj    = dict_sia[sia_name]
+    SIA_COVER  = gdata.var_params['SIA_cover_{:s}'.format(sia_name)]
     start_val  = sia_obj['date']
     age_min    = sia_obj['age_min']
     age_max    = sia_obj['age_max']
@@ -76,6 +75,15 @@ def campaignBuilder():
 
     camp_module.add(IV_SIA(pdict))
 
+  # Add infectivity seasonality
+  all_nodes  = [node_dict[val] for val in node_opts]
+  pdict      = {'startday':       TIME_START ,
+                'nodes':          all_nodes ,
+                'peak_size':      PEAK_SIZE ,
+                'peak_wide':      45.0 ,
+                'peak_time':      PEAK_TIME }
+
+  camp_module.add(IV_BumpR0(pdict))
 
 
   #  ***** End file construction *****
@@ -151,6 +159,54 @@ def IV_SIA(params=dict()):
   camp_iv01.Acquire_Config                  = camp_wane
 
   camp_wane.Initial_Effect                  = 1.0
+
+  return camp_event
+
+#********************************************************************************
+
+# Seasonal infectivity forcing; snaggletooth
+def IV_BumpR0(params=dict()):
+
+  SCHEMA_PATH   =  gdata.schema_path
+
+  camp_event = s2c.get_class_with_defaults('CampaignEvent',             SCHEMA_PATH)
+  camp_coord = s2c.get_class_with_defaults('StandardEventCoordinator',  SCHEMA_PATH)
+  camp_iv01  = s2c.get_class_with_defaults('NodeInfectivityMult',       SCHEMA_PATH)
+
+  node_set   = utils.do_nodes(SCHEMA_PATH, params['nodes'])
+
+  camp_event.Event_Coordinator_Config       = camp_coord
+  camp_event.Start_Day                      = params['startday']
+  camp_event.Nodeset_Config                 = node_set
+
+  camp_coord.Intervention_Config            = camp_iv01
+  camp_coord.Number_Repetitions             =  -1       # Repeat forever
+  camp_coord.Timesteps_Between_Repetitions  = 365.0
+
+  init_off =  params['startday']           %365.0
+  peak_wid =  params['peak_wide']
+  x_peak   = (params['peak_time']-init_off)%365.0
+  y_peak   =  params['peak_size']
+  x_init   = (x_peak             -peak_wid)%365.0
+  x_ends   = (x_peak             +peak_wid)%365.0
+
+  if(peak_wid > x_peak):
+    dx_val = x_peak
+  elif(peak_wid > 365.0-x_peak):
+    dx_val = 365.0-x_peak
+  else:
+    dx_val = peak_wid
+
+  y_bound  = y_peak - (y_peak-1)/peak_wid * dx_val
+  xyvals   = set([(   0.0, y_bound),
+                  (x_init,     1.0),
+                  (x_peak,  y_peak),
+                  (x_ends,     1.0),
+                  ( 365.0, y_bound)])
+  xyvals   = sorted(list(xyvals), key=lambda val: val[0])
+
+  camp_iv01.Multiplier_By_Duration.Times      = [val[0] for val in xyvals]
+  camp_iv01.Multiplier_By_Duration.Values     = [val[1] for val in xyvals]
 
   return camp_event
 
