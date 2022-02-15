52cae82ca0569a46e4026d0cd57e4970b8f010dc
Kurt Frey <kfrey@idmod.org>
2022-02-14 19:30:57 -0800

    Update covariance example to control for R0 variance

--- workflow_covariance01/figure_distributions01/make_fig_distributions.py
+++ workflow_covariance01/figure_distributions01/make_fig_distributions.py
@@ -21,10 +21,11 @@ axs00.tick_params(labelcolor='w', top=False, bottom=False, left=False, right=Fal
 #axs00.set_title('')
 
 rep_set  = [ 221, 222, 223, 224]
-acq_set  = [ 0.0, 0.5, 0.5, 0.5]
-inf_set  = [ 0.5, 0.5, 0.5, 0.5]
-cor_set  = [ 0.0, 0.0, 0.5, 1.0]
-clr_set  = [   0,   1,   2,   3]
+inf_set  = [ 0.5, 0.5, 0.388, 0.137]
+acq_set  = [ 0.0, 0.5, 0.5,   0.5  ]
+cor_set  = [ 0.0, 0.0, 0.4,   0.8  ]
+clr_set  = [ 0,   1,   2,     3    ]
+
 
 for k1 in range(len(rep_set)):
   axs01 = fig01.add_subplot(rep_set[k1],label=None)
@@ -34,35 +35,40 @@ for k1 in range(len(rep_set)):
   axs01.grid(visible=True, which='minor', ls=':', lw=0.1)
   axs01.set_axisbelow(True)
 
-  axs01.set_xlabel('Acquision Risk')
-  axs01.set_ylabel('Transmit Risk')
+  axs01.set_xlabel('Acquision Rate Multiplier')
+  axs01.set_ylabel('Transmission Rate Multiplier')
 
   axs01.set_xlim(0,6)
   axs01.set_ylim(0,6)
 
+  R0_VAR   = inf_set[k1]
   AQ_VAR   = acq_set[k1]
-  BI_SIG   = inf_set[k1]
   rho      = cor_set[k1]
 
-  LNSIG  = np.sqrt(np.log(AQ_VAR+1.0))
-  LNMU   = -0.5*LNSIG*LNSIG
-  BIMU   = -0.5*BI_SIG*BI_SIG
+  R0_LN_SIG   = np.sqrt(np.log(R0_VAR+1.0))
+  R0_LN_MU    = -0.5*R0_LN_SIG*R0_LN_SIG
 
-  risk_vec   = np.random.lognormal(mean=LNMU, sigma=LNSIG,  size=N)
-  inf_vec    = np.random.lognormal(mean=BIMU, sigma=BI_SIG, size=N)
+  ACQ_LN_SIG  = np.sqrt(np.log(AQ_VAR+1.0))
+  ACQ_LN_MU   = -0.5*ACQ_LN_SIG*ACQ_LN_SIG
+
+  risk_vec   = np.random.lognormal(mean=ACQ_LN_MU, sigma=ACQ_LN_SIG, size=N)
+  inf_vec    = np.random.lognormal(mean=R0_LN_MU,  sigma=R0_LN_SIG,  size=N)
   corr_vec   = inf_vec*(1 + rho*(risk_vec - 1))
 
   #print(np.mean(risk_vec),np.var(risk_vec))
   #print(np.mean(inf_vec), np.var(inf_vec))
   #print(np.mean(corr_vec),np.var(corr_vec))
+  #print()
   #print(np.cov(risk_vec,corr_vec))
+  #print()
+  #print()
 
   axs01.plot(risk_vec,corr_vec,marker='.',lw=0.0,color='C{:d}'.format(clr_set[k1]))
 
 
+plt.tight_layout()
 plt.savefig('fig_distributions01.png')
-#plt.savefig('fig_distributions01.svg')
-
 plt.close()
+
 
 #*******************************************************************************
