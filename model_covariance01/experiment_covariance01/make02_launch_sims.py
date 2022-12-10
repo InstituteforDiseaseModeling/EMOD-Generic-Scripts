855465afedf57504212b78ef9d1d3e54d6810e45
Kurt Frey <kfrey@idmod.org>
2022-12-10 18:20:35 -0500

    Use local path definition

--- model_covariance01/experiment_covariance01/make02_launch_sims.py
+++ model_covariance01/experiment_covariance01/make02_launch_sims.py
@@ -11,7 +11,7 @@ from idmtools.core.id_file            import write_id_file
 # Ought to go in emodpy
 import sys
 LOCAL_PATH = os.path.abspath(os.path.join('..','..','local_python'))
-sys.path.insert(0, os.path.join('..','..','local_python'))
+sys.path.insert(0, LOCAL_PATH)
 from emod_exp import exp_from_def_file
 
 # ******************************************************************************
