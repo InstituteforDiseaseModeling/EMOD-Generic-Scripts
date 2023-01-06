7c78f3510ef0daba0d183362b21b2ec59765cdf5
Kurt Frey <kfrey@idmod.org>
2023-01-05 20:56:42 -0500

    Add DHS birthrates

---
+++ model_measles_cod01/Assets/python/refdat_birthrate.py
@@ -0,0 +1,70 @@
+#********************************************************************************
+#
+# Birth rate data
+#
+#********************************************************************************
+
+data_dict = \
+{
+   "AFRO:DRCONGO:BAS_UELE":
+     39.7/1000.0/365.0,
+   "AFRO:DRCONGO:EQUATEUR":
+     45.8/1000.0/365.0,
+   "AFRO:DRCONGO:HAUT_KATANGA":
+     49.0/1000.0/365.0,
+   "AFRO:DRCONGO:HAUT_LOMAMI":
+     49.0/1000.0/365.0,
+   "AFRO:DRCONGO:HAUT_UELE":
+     39.7/1000.0/365.0,
+   "AFRO:DRCONGO:ITURI":
+     39.7/1000.0/365.0,
+   "AFRO:DRCONGO:KASAI":
+     53.0/1000.0/365.0,
+   "AFRO:DRCONGO:KASAI_CENTRAL":
+     53.0/1000.0/365.0,
+   "AFRO:DRCONGO:KASAI_ORIENTAL":
+     46.5/1000.0/365.0,
+   "AFRO:DRCONGO:KINSHASA":
+     36.1/1000.0/365.0,
+   "AFRO:DRCONGO:KONGO_CENTRAL":
+     43.6/1000.0/365.0,
+   "AFRO:DRCONGO:KWANGO":
+     42.4/1000.0/365.0,
+   "AFRO:DRCONGO:KWILU":
+     42.4/1000.0/365.0,
+   "AFRO:DRCONGO:LOMAMI":
+     46.5/1000.0/365.0,
+   "AFRO:DRCONGO:LUALABA":
+     49.0/1000.0/365.0,
+   "AFRO:DRCONGO:MAI_NDOMBE":
+     42.4/1000.0/365.0,
+   "AFRO:DRCONGO:MANIEMA":
+     39.1/1000.0/365.0,
+   "AFRO:DRCONGO:MONGALA":
+     45.8/1000.0/365.0,
+   "AFRO:DRCONGO:NORD_KIVU":
+     40.9/1000.0/365.0,
+   "AFRO:DRCONGO:NORD_UBANGI":
+     45.8/1000.0/365.0,
+   "AFRO:DRCONGO:SANKURU":
+     46.5/1000.0/365.0,
+   "AFRO:DRCONGO:SUD_KIVU":
+     48.8/1000.0/365.0,
+   "AFRO:DRCONGO:SUD_UBANGI":
+     45.8/1000.0/365.0,
+   "AFRO:DRCONGO:TANGANYIKA":
+     49.0/1000.0/365.0,
+   "AFRO:DRCONGO:TSHOPO":
+     39.7/1000.0/365.0,
+   "AFRO:DRCONGO:TSHUAPA":
+     45.8/1000.0/365.0
+}
+
+#********************************************************************************
+
+
+
+
+
+
+
