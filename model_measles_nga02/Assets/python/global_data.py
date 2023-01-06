c7af39883b765f4832f4f7b6a0317cd4df26d022
Kurt Frey <kfrey@idmod.org>
2023-01-05 20:58:14 -0500

    Reorder global params

--- model_measles_nga02/Assets/python/global_data.py
+++ model_measles_nga02/Assets/python/global_data.py
@@ -9,6 +9,12 @@ sim_index       = None
 var_params      = dict()
 schema_path     = 'Assets/schema.json'
 
+start_year      = 2015.0
+start_log       =  365.0*(2015-1900)
+base_year       = 1900.0
+
+max_clock       =  180.0
+
 # Filename params
 demog_files     = list()
 camp_file       = None
@@ -20,16 +26,10 @@ demog_min_pop   =    1
 # Other stuff
 first_call_bool = True
 
-init_pop        = None
+init_pop        =    0
 
 brate_mult_x    = None
 brate_mult_y    = None
-
-start_log       = 365.0*(2015-1900)
-
-base_year       = 1900.0
-start_year      = 2015.0
-max_clock       =  180.0
 
 #*******************************************************************************
 
