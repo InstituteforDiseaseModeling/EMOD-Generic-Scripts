ff4e090284e37dea79c334340e606aa0331aeccf
Kurt Frey <kfrey@idmod.org>
2023-03-30 16:09:05 -0700

    Campaign updates to Ghana model

--- model_measles_gha01/Assets/python/builder_campaign.py
+++ model_measles_gha01/Assets/python/builder_campaign.py
@@ -141,7 +141,7 @@ def IV_MCV1(params=dict()):
   camp_iv01  = s2c.get_class_with_defaults('NodeLevelHealthTriggeredIVScaleUpSwitch',  SCHEMA_PATH)
   camp_iv02  = s2c.get_class_with_defaults('DelayedIntervention',                      SCHEMA_PATH)
   camp_iv03  = s2c.get_class_with_defaults('Vaccine',                                  SCHEMA_PATH)
-  camp_wane  = s2c.get_class_with_defaults('WaningEffectConstant',                     SCHEMA_PATH)
+  camp_wane  = s2c.get_class_with_defaults('WaningEffect',                             SCHEMA_PATH)
 
   node_set   = utils.do_nodes(SCHEMA_PATH, params['nodes'])
 
@@ -180,7 +180,7 @@ def IV_SIA(params=dict()):
   camp_event = s2c.get_class_with_defaults('CampaignEvent',             SCHEMA_PATH)
   camp_coord = s2c.get_class_with_defaults('StandardEventCoordinator',  SCHEMA_PATH)
   camp_iv01  = s2c.get_class_with_defaults('Vaccine',                   SCHEMA_PATH)
-  camp_wane  = s2c.get_class_with_defaults('WaningEffectConstant',      SCHEMA_PATH)
+  camp_wane  = s2c.get_class_with_defaults('WaningEffect',              SCHEMA_PATH)
 
   node_set   = utils.do_nodes(SCHEMA_PATH, params['nodes'])
 
