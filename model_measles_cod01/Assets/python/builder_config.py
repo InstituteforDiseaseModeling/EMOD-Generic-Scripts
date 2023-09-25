4e1356e6148c28e17e21fb54e0a91cd59b406841
Kurt Frey <kfrey@idmod.org>
2023-09-24 18:37:52 -0700

    Updates for api change

--- model_measles_cod01/Assets/python/builder_config.py
+++ model_measles_cod01/Assets/python/builder_config.py
@@ -111,12 +111,11 @@ def update_config_obj(config):
   config.parameters.Post_Infection_Transmission_Multiplier           =   0.0
   config.parameters.Post_Infection_Mortality_Multiplier              =   0.0
 
-  config.parameters.Enable_Maternal_Protection                       =   1
-  config.parameters.Susceptibility_Type                              = 'BINARY'
-  config.parameters.Maternal_Protection_Type                         = 'SIGMOID'
-  config.parameters.Maternal_Sigmoid_SteepFac                        =  50.0
-  config.parameters.Maternal_Sigmoid_HalfMaxAge                      = 150.0
-  config.parameters.Maternal_Sigmoid_SusInit                         =   0.0
+  config.parameters.Maternal_Acquire_Config.Initial_Effect                    =   1.0
+  config.parameters.Maternal_Acquire_Config.Enable_Box_Duration_Distribution  =   1
+  config.parameters.Maternal_Acquire_Config.Box_Duration_Distribution         = 'GAUSSIAN_DISTRIBUTION'
+  config.parameters.Maternal_Acquire_Config.Box_Duration_Gaussian_Mean        = 150.0
+  config.parameters.Maternal_Acquire_Config.Box_Duration_Gaussian_Std_Dev     =  80.0
 
   config.parameters.Enable_Initial_Susceptibility_Distribution       =   1
   config.parameters.Susceptibility_Initialization_Distribution_Type  = 'DISTRIBUTION_COMPLEX'
