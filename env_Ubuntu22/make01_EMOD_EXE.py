40e91732ae4d26719b82e5582f7423f156fb40ca
Kurt Frey <kfrey@idmod.org>
2023-12-06 14:47:48 -0800

    Updates for Ubuntu22

--- env_Ubuntu22/make01_EMOD_EXE.py
+++ env_Ubuntu22/make01_EMOD_EXE.py
@@ -2,69 +2,17 @@
 #
 #*******************************************************************************
 
-import os, sys, json
+import os, sys
 
-from idmtools.core.platform_factory      import Platform
-from idmtools.core.id_file               import write_id_file
-from idmtools.entities.command_task      import CommandTask
-from idmtools.entities.experiment        import Experiment
-from idmtools.assets                     import AssetCollection
-from idmtools_platform_comps.utils.singularity_build \
-                                         import SingularityBuildWorkItem
-from idmtools_platform_comps.utils.assetize_output.assetize_output \
-                                         import AssetizeOutput
-
-# ******************************************************************************
-
-# Run work items on COMPS
-def make_work():
-
-  # Prepare the platform
-  plat_obj = Platform(block            = 'COMPS',
-                      endpoint         = 'https://comps.idmod.org',
-                      environment      = 'Calculon',
-                      priority         = 'Highest',
-                      simulation_root  = '$COMPS_PATH(USER)',
-                      node_group       = 'idm_48cores',
-                      num_cores        = '1',
-                      num_retries      = '0',
-                      exclusive        = 'False')
-
-  # Add image for base OS
-  os_image = AssetCollection.from_id_file('EMOD_OS.id')
-
-  # Creates a work item to create the build image
-  sbwi_obj = SingularityBuildWorkItem(name             = 'Build_EMOD_EXE_Ubuntu22',
-                                      definition_file  = 'EMOD_EXE_Ubuntu22.def',
-                                      force            = True)
-  sbwi_obj.assets.add_assets(os_image)
-
-  # Wait until the build image is finished
-  ac_obj = sbwi_obj.run(wait_on_done=True, platform=plat_obj)
-
-  # Magic words
-  cmd_line = 'singularity exec Assets/EMOD_EXE_Ubuntu22.sif cp -r /outputs/. .'
-
-  # Create CommandTask
-  task_obj = CommandTask(command=cmd_line)
-  task_obj.common_assets.add_assets(ac_obj)
-
-  # Wait until asset collection has been built
-  ao_obj = AssetizeOutput(no_simulation_prefix = True)
-  ao_obj.from_items(Experiment.from_task(task_obj))
-  ao_obj.run(wait_on_done=True, platform=plat_obj)
-
-  # Save asset collection id to file
-  write_id_file('EMOD_EXE.id', ao_obj.asset_collection)
-  print()
-  print(ao_obj.uid.hex)
-
-  return None
+# Ought to go in emodpy
+LOCAL_PATH = os.path.abspath(os.path.join('..','local_python'))
+sys.path.insert(0, LOCAL_PATH)
+from emod_build  import  make_EXE_asset
 
 # ******************************************************************************
 
 if(__name__ == "__main__"):
 
-  make_work()
+  make_EXE_asset()
 
 # ******************************************************************************
