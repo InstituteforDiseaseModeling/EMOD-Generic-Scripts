2fe07281a030ab45d253c8f7119ba0c40e4051ef
Kurt Frey <kfrey@idmod.org>
2024-01-21 18:07:53 -0800

    Spacing

--- model_measles_nga01/Assets/python/global_data.py
+++ model_measles_nga01/Assets/python/global_data.py
@@ -19,20 +19,22 @@ demog_node      = dict()
 demog_node_map  = dict()      # LGA:     [NodeIDs]
 
 demog_object    = None
-demog_min_pop   = 100
+demog_min_pop   =  100
 
 # Other stuff
 first_call_bool = True
-prev_proc_time  = -1.0
-max_node_id     = 0
+prev_proc_time  =   -1.0
+max_node_id     =    0
 
 data_vec_time   = None
 data_vec_node   = None
 data_vec_mcw    = None
 
-base_year       = 1900
+base_year       = 1900.0
+t_step_days     =    1.0
+start_log_time  = None
 
-max_clock       = 120.0
+max_clock       =  120.0
 
 #*******************************************************************************
 
