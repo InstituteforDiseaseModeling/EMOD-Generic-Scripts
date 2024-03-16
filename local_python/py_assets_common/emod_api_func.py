5bdf0249238780d8ecd4a54173e6b5376e4e2668
Kurt Frey <kfrey@idmod.org>
2024-03-15 21:56:07 -0700

    Linting! Kind of hate it.

--- local_python/py_assets_common/emod_api_func.py
+++ local_python/py_assets_common/emod_api_func.py
@@ -1,105 +1,99 @@
-#********************************************************************************
+# *****************************************************************************
 #
-#********************************************************************************
+# *****************************************************************************
 
-import os, sys, json, time
+import json
+import os
+import time
 
 import global_data as gdata
 
-from builder_config         import configBuilder
-from builder_demographics   import demographicsBuilder
-from builder_campaign       import campaignBuilder
-from builder_dlls           import dllcBuilder
+from builder_config import configBuilder
+from builder_demographics import demographicsBuilder
+from builder_campaign import campaignBuilder
+from builder_dlls import dllcBuilder
 
 import numpy as np
 
 from emod_api import __version__ as API_CUR
 
-#********************************************************************************
+# *****************************************************************************
+
 
 def standard_pre_process():
 
-  API_MIN = '1.30.0'
-  PFILE   = 'param_dict.json'
-  I_FILE  = 'idx_str_file.txt'
-  EXP_C   = 'EXP_CONSTANT'
-  EXP_V   = 'EXP_VARIABLE'
+    API_MIN = '1.30.0'
+    PFILE = 'param_dict.json'
+    I_FILE = 'idx_str_file.txt'
+    EXP_C = 'EXP_CONSTANT'
+    EXP_V = 'EXP_VARIABLE'
 
+    # Declare current version of emod-api; check min version
+    print('Using emod-api {:s}'.format(API_CUR))
+    for ver_val in zip(API_CUR.split('.'), API_MIN.split('.')):
+        if (ver_val[0] > ver_val[1]):
+            break
+        if (ver_val[0] == ver_val[1]):
+            continue
+        if (ver_val[0] < ver_val[1]):
+            err_text = 'Using emod-api {:s}; minimum version is {:s}'
+            raise Exception(err_text.format(API_CUR, API_MIN))
 
-  # Declare current version of emod-api; check min version
-  print('Using emod-api {:s}'.format(API_CUR))
-  for ver_val in zip(API_CUR.split('.'),API_MIN.split('.')):
-    if(ver_val[0] >  ver_val[1]):
-      break
-    if(ver_val[0] == ver_val[1]):
-      continue
-    if(ver_val[0] <  ver_val[1]):
-      raise Exception('Using emod-api {:s}; minimum version is {:s}'.format(API_CUR,API_MIN))
+    # Read index of simulation parameter set
+    with open(I_FILE) as fid01:
+        sim_index = int(fid01.readline())
+    gdata.sim_index = sim_index
 
+    # Read parameter dictionary file
+    param_dict = dict()
+    param_paths = ['.', 'Assets']
 
-  # Read index of simulation parameter set
-  with open(I_FILE) as fid01:
-    sim_index = int(fid01.readline())
-  gdata.sim_index = sim_index
+    for ppath in param_paths:
+        pfileopt = os.path.join(ppath, PFILE)
+        if (os.path.exists(pfileopt)):
+            with open(pfileopt) as fid01:
+                param_dict = json.load(fid01)
+            break
 
+    # Validation checks on parameter dictionary file
+    if (not param_dict):
+        raise Exception('No {:s} found'.format(PFILE))
 
-  # Read parameter dictionary file
-  param_dict  = dict()
-  param_paths = ['.','Assets']
+    names_variable = set(param_dict[EXP_V].keys())
+    names_constant = set(param_dict[EXP_C].keys())
+    if (names_constant.intersection(names_variable)):
+        err_text = 'Variable name in both {:s} and {:s}'
+        raise Exception(err_text.format(EXP_C, EXP_V))
 
-  for ppath in param_paths:
-    pfileopt = os.path.join(ppath,PFILE)
-    if(os.path.exists(pfileopt)):
-      with open(pfileopt) as fid01:
-        param_dict = json.load(fid01)
-      break
+    # Select simulation parameters from parameters dictionary, save in globals
+    var_params = dict()
 
+    var_params.update({keyval: param_dict[EXP_V][keyval][sim_index]
+                       for keyval in param_dict[EXP_V]})
+    var_params.update({keyval: param_dict[EXP_C][keyval]
+                       for keyval in param_dict[EXP_C]})
 
-  # Validation checks on parameter dictionary file
-  if(not param_dict):
-    raise Exception('No {:s} found'.format(PFILE))
+    gdata.var_params = var_params
 
-  names_variable = set(param_dict[EXP_V].keys())
-  names_constant = set(param_dict[EXP_C].keys())
-  if(names_constant.intersection(names_variable)):
-    raise Exception('Variable name in both {:s} and {:s}'.format(EXP_C,EXP_V))
+    # Seed random number generator
+    np.random.seed(sim_index)
 
+    # Demographics file
+    demographicsBuilder()
+    time.sleep(1)
 
-  # Select simulation parameters from parameters dictionary, save in globals
-  var_params  = dict()
+    # Campaign interventions file
+    campaignBuilder()
+    time.sleep(1)
 
-  var_params.update({keyval:param_dict[EXP_V][keyval][sim_index]
-                  for keyval in param_dict[EXP_V]})
-  var_params.update({keyval:param_dict[EXP_C][keyval]
-                  for keyval in param_dict[EXP_C]})
+    # Custom reporter file
+    dllcBuilder()
+    time.sleep(1)
 
-  gdata.var_params = var_params
+    # Simulation configuration file
+    config_filename = configBuilder()
+    time.sleep(1)
 
+    return config_filename
 
-  # Seed random number generator
-  np.random.seed(sim_index)
-
-
-  # Demographics file
-  demographicsBuilder()
-  time.sleep(1)
-
-
-  # Campaign interventions file
-  campaignBuilder()
-  time.sleep(1)
-
-
-  # Custom reporter file
-  dllcBuilder()
-  time.sleep(1)
-
-
-  # Simulation configuration file
-  config_filename = configBuilder()
-  time.sleep(1)
-
-
-  return config_filename
-
-#********************************************************************************
+# *****************************************************************************
