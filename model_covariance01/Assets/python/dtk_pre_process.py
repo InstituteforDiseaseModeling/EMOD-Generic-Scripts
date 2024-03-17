1197ad6f9317fb57863ee997ca01e8f8c0683f7f
Kurt Frey <kfrey@idmod.org>
2024-03-17 12:25:36 -0700

    Standardize pre-process script

--- model_covariance01/Assets/python/dtk_pre_process.py
+++ model_covariance01/Assets/python/dtk_pre_process.py
@@ -1,17 +1,18 @@
-#********************************************************************************
+# *****************************************************************************
 #
-#********************************************************************************
+# *****************************************************************************
 
-from emod_api_func   import   standard_pre_process
+from emod_api_func import standard_pre_process
 
-#*******************************************************************************
+# *****************************************************************************
+
 
 def application(config_filename_in):
 
-  # Standard pre-process function calls all other file builders
-  config_filename = standard_pre_process()
+    # Standard pre-process function calls all other file builders
+    config_filename = standard_pre_process()
 
-  # Pre-process function needs to return config filename as a string
-  return config_filename
+    # Pre-process function needs to return config filename as a string
+    return config_filename
 
-#*******************************************************************************
+# *****************************************************************************
