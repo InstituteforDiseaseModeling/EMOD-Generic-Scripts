e2e53701bbea69d394ce91107def0bd904fb3a03
Kurt Frey <kfrey@idmod.org>
2024-03-21 14:37:41 -0700

    Move vd calc to shared script

--- local_python/py_assets_common/emod_demog_func.py
+++ local_python/py_assets_common/emod_demog_func.py
@@ -5,8 +5,10 @@
 import json
 import os
 
+import numpy as np
+
 from emod_constants import DEMOG_FILE, PATH_OVERLAY, \
-                           MORT_XVAL, POP_AGE_DAYS
+                           MORT_XVAL, POP_AGE_DAYS, MAX_DAILY_MORT
 
 # *****************************************************************************
 
@@ -60,5 +62,57 @@ def demog_vd_over(ref_name, node_list, cb_rate,
         json.dump(vd_over_dict, fid01)
 
     return nfname
+
+# *****************************************************************************
+
+
+def demog_vd_calc(year_vec, year_init, pop_mat, pop_init):
+
+    # Calculate vital dynamics
+    diff_ratio = (pop_mat[:-1, :-1]-pop_mat[1:, 1:])/pop_mat[:-1, :-1]
+    t_delta = np.diff(year_vec)
+    pow_vec = 365.0*t_delta
+    mortvecs = 1.0-np.power(1.0-diff_ratio, 1.0/pow_vec)
+    mortvecs = np.minimum(mortvecs, MAX_DAILY_MORT)
+    mortvecs = np.maximum(mortvecs, 0.0)
+    tot_pop = np.sum(pop_mat, axis=0)
+    tpop_mid = (tot_pop[:-1]+tot_pop[1:])/2.0
+    pop_corr = np.exp(-mortvecs[0, :]*pow_vec/2.0)
+
+    brate_vec = np.round(pop_mat[0, 1:]/tpop_mid/t_delta*1000.0, 1)/pop_corr
+    brate_val = np.interp(year_init, year_vec[:-1], brate_vec)
+    yrs_off = year_vec[:-1]-year_init
+    yrs_dex = (yrs_off > 0)
+
+    brmultx_01 = np.array([0.0] + (365.0*yrs_off[yrs_dex]).tolist())
+    brmulty_01 = np.array([1.0] + (brate_vec[yrs_dex]/brate_val).tolist())
+    brmultx_02 = np.zeros(2*len(brmultx_01)-1)
+    brmulty_02 = np.zeros(2*len(brmulty_01)-1)
+
+    brmultx_02[0::2] = brmultx_01[0:]
+    brmulty_02[0::2] = brmulty_01[0:]
+    brmultx_02[1::2] = brmultx_01[1:]-0.5
+    brmulty_02[1::2] = brmulty_01[0:-1]
+
+    age_init_cdf = np.cumsum(pop_init[:-1])/np.sum(pop_init)
+    age_x = [0] + age_init_cdf.tolist()
+
+    birth_rate = brate_val/365.0/1000.0
+    mort_year = np.zeros(2*year_vec.shape[0]-3)
+
+    mort_year[0::2] = year_vec[0:-1]
+    mort_year[1::2] = year_vec[1:-1]-1e-4
+    mort_year = mort_year.tolist()
+
+    mort_mat = np.zeros((len(MORT_XVAL), len(mort_year)))
+
+    mort_mat[0:-2:2, 0::2] = mortvecs
+    mort_mat[1:-2:2, 0::2] = mortvecs
+    mort_mat[0:-2:2, 1::2] = mortvecs[:, :-1]
+    mort_mat[1:-2:2, 1::2] = mortvecs[:, :-1]
+    mort_mat[-2:, :] = MAX_DAILY_MORT
+
+    return (mort_year, mort_mat, age_x, birth_rate, brmultx_02, brmulty_02)
+
 
 # *****************************************************************************
--- model_demographics02/Assets/python/builder_demographics.py
+++ model_demographics02/Assets/python/builder_demographics.py
@@ -12,8 +12,8 @@ from emod_api.demographics.Demographics import Demographics, Node
 
 import numpy as np
 
-from emod_demog_func import demog_vd_over
-from emod_constants import DEMOG_FILE, MORT_XVAL, MAX_DAILY_MORT
+from emod_demog_func import demog_vd_calc, demog_vd_over
+from emod_constants import DEMOG_FILE
 
 # *****************************************************************************
 
@@ -50,51 +50,17 @@ def demographicsBuilder():
     demog_obj.raw['Defaults']['NodeAttributes'].clear()
 
     # Calculate vital dynamics
-    diff_ratio = (pop_mat[:-1, :-1]-pop_mat[1:, 1:])/pop_mat[:-1, :-1]
-    t_delta = np.diff(year_vec)
-    pow_vec = 365.0*t_delta
-    mortvecs = 1.0-np.power(1.0-diff_ratio, 1.0/pow_vec)
-    mortvecs = np.minimum(mortvecs, MAX_DAILY_MORT)
-    mortvecs = np.maximum(mortvecs, 0.0)
-    tot_pop = np.sum(pop_mat, axis=0)
-    tpop_mid = (tot_pop[:-1]+tot_pop[1:])/2.0
-    pop_corr = np.exp(-mortvecs[0, :]*pow_vec/2.0)
+    vd_tup = demog_vd_calc(year_vec, year_init, pop_mat, pop_init)
 
-    brate_vec = np.round(pop_mat[0, 1:]/tpop_mid/t_delta*1000.0, 1)/pop_corr
-    brate_val = np.interp(year_init, year_vec[:-1], brate_vec)
-    yrs_off = year_vec[:-1]-year_init
-    yrs_dex = (yrs_off > 0)
+    mort_year = vd_tup[0]
+    mort_mat = vd_tup[1]
+    age_x = vd_tup[2]
+    birth_rate = vd_tup[3]
+    br_mult_x = vd_tup[4]
+    br_mult_y = vd_tup[5]
 
-    brmultx_01 = np.array([0.0] + (365.0*yrs_off[yrs_dex]).tolist())
-    brmulty_01 = np.array([1.0] + (brate_vec[yrs_dex]/brate_val).tolist())
-    brmultx_02 = np.zeros(2*len(brmultx_01)-1)
-    brmulty_02 = np.zeros(2*len(brmulty_01)-1)
-
-    brmultx_02[0::2] = brmultx_01[0:]
-    brmulty_02[0::2] = brmulty_01[0:]
-    brmultx_02[1::2] = brmultx_01[1:]-0.5
-    brmulty_02[1::2] = brmulty_01[0:-1]
-
-    gdata.brate_mult_x = brmultx_02.tolist()
-    gdata.brate_mult_y = brmulty_02.tolist()
-
-    age_init_cdf = np.cumsum(pop_init[:-1])/np.sum(pop_init)
-    age_x = [0] + age_init_cdf.tolist()
-
-    birth_rate = brate_val/365.0/1000.0
-    mort_year = np.zeros(2*year_vec.shape[0]-3)
-
-    mort_year[0::2] = year_vec[0:-1]
-    mort_year[1::2] = year_vec[1:-1]-1e-4
-    mort_year = mort_year.tolist()
-
-    mort_mat = np.zeros((len(MORT_XVAL), len(mort_year)))
-
-    mort_mat[0:-2:2, 0::2] = mortvecs
-    mort_mat[1:-2:2, 0::2] = mortvecs
-    mort_mat[0:-2:2, 1::2] = mortvecs[:, :-1]
-    mort_mat[1:-2:2, 1::2] = mortvecs[:, :-1]
-    mort_mat[-2:, :] = MAX_DAILY_MORT
+    gdata.brate_mult_x = br_mult_x.tolist()
+    gdata.brate_mult_y = br_mult_y.tolist()
 
     # Write vital dynamics overlay
     n_list = [node_obj.forced_id for node_obj in node_list]
