3863f9013cefe0e90d3c668720dab7ee00c76871
Kurt Frey <kfrey@idmod.org>
2024-03-28 11:05:33 -0700

    Figure script working updates

--- model_demographics01/figure_pyramids/make_fig_pyramids.py
+++ model_demographics01/figure_pyramids/make_fig_pyramids.py
@@ -7,11 +7,6 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-# from dtk_post_process import tpop_yval
-
-# from dtk_post_process import uk_1950_frac, uk_1960_frac
-# from dtk_post_process import uk_1970_frac, uk_1980_frac
-
 # Ought to go in emodpy
 LOCAL_PATH = os.path.abspath(os.path.join('..', '..', 'local_python'))
 sys.path.insert(0, LOCAL_PATH)
@@ -53,18 +48,23 @@ def make_fig():
 
     fidx = (pyr_mat[:, 0, 0] >= 0)
 
+    # Reference data
     popfile = 'pop_dat_GBR.csv'
     fname_pop = os.path.join('..', 'Assets', 'data', popfile)
     pop_input = np.loadtxt(fname_pop, dtype=int, delimiter=',')
+    year_vec = pop_input[0, :]
+    pop_mat = pop_input[1:, :]
 
-    year_vec_dat = pop_input[0, :]
-    pop_mat_dat = pop_input[1:, :]
+    pop_mat_ref = np.zeros((pop_mat.shape[0], 30+1))
+    for yr_idx in range(30+1):
+        yr_val = yr_idx + 1950
+        pop_vec_ref = [np.interp(yr_val, year_vec, pop_mat[idx, :])
+                       for idx in range(pop_mat.shape[0])]
+        pop_mat_ref[:, yr_idx] = pop_vec_ref
 
     p_tuple = [(VAR_SET[k1], AGE_SET[k1], MORT_SET[k1])
                for k1 in range(N_SIMS)]
     p_tuple = sorted(list(set(p_tuple)))
-
-    # x_dat_list = [uk_1950_frac, uk_1960_frac, uk_1970_frac, uk_1980_frac]
 
     for k0 in range(len(p_tuple)):
         fig01 = plt.figure(figsize=(24, 12))
@@ -76,9 +76,9 @@ def make_fig():
         pyr_mat_avg = np.mean(pyr_mat[gidx, :, :], axis=0)
         pyr_mat_std = np.std(pyr_mat[gidx, :, :], axis=0)
 
-        # Figures - Sims
         for k1 in range(0, pyr_mat_avg.shape[0], 10):
 
+            # Figures - Sims
             axs01 = fig01.add_subplot(2, 4, int(k1/10)+1, label=None)
             plt.sca(axs01)
 
@@ -118,40 +118,40 @@ def make_fig():
             axs01.text(-7, 92.5, '{:04d}'.format(1950+k1), fontsize=18)
             axs01.text(3, 87.5, txt_str, fontsize=18)
 
-        # Figures - Reference
-        # for k1 in range(len(x_dat_list)):
+            # Figures - Reference
+            axs01 = fig01.add_subplot(2, 4, int(k1/10)+1+4, label=None)
+            plt.sca(axs01)
 
-        #    axs01 = fig01.add_subplot(int('24{:d}'.format(k1+4+1)), label=None)
-        #    plt.sca(axs01)
+            axs01.grid(visible=True, which='major', ls='-', lw=0.5, label='')
+            axs01.grid(visible=True, which='minor', ls=':', lw=0.1)
+            axs01.set_axisbelow(True)
 
-        #    axs01.grid(visible=True, which='major', ls='-', lw=0.5, label='')
-        #    axs01.grid(visible=True, which='minor', ls=':', lw=0.1)
-        #    axs01.set_axisbelow(True)
+            axs01.set_xlabel('Percentage', fontsize=14)
+            axs01.set_ylabel('Age (yrs)', fontsize=14)
 
-        #    axs01.set_xlabel('Percentage', fontsize=14)
-        #    axs01.set_ylabel('Age (yrs)', fontsize=14)
+            if (k1 == 30):
+                axs02 = axs01.twinx()
+                axs02.set_ylabel('Reference', fontsize=24)
+                axs02.set_yticks(ticks=[0, 1])
+                axs02.set_yticklabels(['', ''])
 
-        #    if(k1 == 3):
-        #        axs02 = axs01.twinx()
-        #        axs02.set_ylabel('Reference', fontsize=24)
-        #        axs02.set_yticks(ticks=[0,1])
-        #        axs02.set_yticklabels(['',''])
+            axs01.set_xlim(-8, 8)
+            axs01.set_ylim(0, 100)
 
-        #    axs01.set_xlim(  -8,   8)
-        #    axs01.set_ylim(   0, 100)
+            axs01.set_xticks(ticks=TLOCS)
+            axs01.set_xticklabels(TLABS)
 
-        #    axs01.set_xticks(ticks=TLOCS)
-        #    axs01.set_xticklabels(TLABS)
+            ydat = np.array(POP_AGE_DAYS)/365.0 - 2.5
+            pop_dat = pop_mat_ref[:, k1]
+            tpop = np.sum(pop_dat)
+            pop_dat = pop_dat[:-1]/tpop
 
-        #    ydat        = np.array(POP_AGE_DAYS)/365.0 - 2.5
-        #    pop_dat     = np.array(x_dat_list[k1])
-        #    effpop      = yref[10*k1]
+            axs01.barh(ydat[1:], 100*pop_dat/2.0, height=4.75, color=CLR_F)
+            axs01.barh(ydat[1:], -100*pop_dat/2.0, height=4.75, color=CLR_M)
 
-        #    axs01.barh(ydat[1:],  100*pop_dat[1:]/2.0, height=4.75, color=CF)
-        #    axs01.barh(ydat[1:], -100*pop_dat[1:]/2.0, height=4.75, color=CM)
-
-        #    axs01.text( -7, 92.5, '{:04d}'.format(1950+10*k1), fontsize=18)
-        #    axs01.text(  3, 87.5, 'Total Pop\n{:6.3f}M'.format(effpop), fontsize=18)
+            txt_str = 'Total Pop\n{:6.3f}M'.format(tpop/1.0e6)
+            axs01.text(-7, 92.5, '{:04d}'.format(1950+k1), fontsize=18)
+            axs01.text(3, 87.5, txt_str, fontsize=18)
 
         # Save figure
         plt.tight_layout()
