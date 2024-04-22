dd680bcc1a6a56f1752fd2e562ef7e2a0329ecd0
Kurt Frey <kfrey@idmod.org>
2024-04-22 12:15:12 -0700

    Linting trends figure

--- model_measles01/figure_trends/make_fig_trends.py
+++ model_measles01/figure_trends/make_fig_trends.py
@@ -1,139 +1,132 @@
-#*******************************************************************************
+# *****************************************************************************
 
-import os, json, sys
+import json
+import os
+import sys
 
-sys.path.append(os.path.join('..','Assets','python'))
+import numpy as np
+import matplotlib.pyplot as plt
 
-import numpy               as np
-import matplotlib.pyplot   as plt
-import matplotlib.patches  as patch
-import matplotlib          as mpl
+# Ought to go in emodpy
+LOCAL_PATH = os.path.join('..', '..', 'local_python', 'py_assets_common')
+sys.path.insert(0, os.path.abspath(LOCAL_PATH))
+sys.path.append(os.path.join('..', 'Assets', 'python'))
+from emod_constants import EXP_C, EXP_V, NUM_SIMS, P_FILE, POP_PYR
+from global_data import run_years
+from dtk_post_process import AGE_HIST_BINS, IHME_MORT_X, IHME_MORT_Y
 
-from global_data           import run_years
-from dtk_post_process      import AGE_HIST_BINS, IHME_MORT_X, IHME_MORT_Y
+# *****************************************************************************
 
-#*******************************************************************************
+DIRNAMES = ['experiment_popL1']
 
-
-DIRNAMES = ['experiment_popL1',
-            'experiment_popL1_MCV2',
-            'experiment_popL2',
-            'experiment_popL2_SIA6MO',
-            'experiment_popL2_SIA9MO',
-            'experiment_popL2_SIA9MO80p',
-            'experiment_popL2_MCV2',
-            'experiment_popL3',
-            'experiment_popL3_MCV2']
+# *****************************************************************************
 
 
-for dirname in DIRNAMES:
+def make_fig():
 
-  # Sim outputs
-  tpath = os.path.join('..',dirname)
+    for dirname in DIRNAMES:
 
-  with open(os.path.join(tpath,'data_brick.json')) as fid01:
-    data_brick = json.load(fid01)
+        # Sim outputs
+        tpath = os.path.join('..', dirname)
 
-  with open(os.path.join(tpath,'param_dict.json')) as fid01:
-    param_dict = json.load(fid01)
+        with open(os.path.join(tpath, 'data_brick.json')) as fid01:
+            data_brick = json.load(fid01)
 
-  nsims        = int(param_dict['NUM_SIMS'])
-  ref_year     = param_dict['EXP_CONSTANT']['start_year']
-  inf_dat      = np.zeros((nsims,12*int(run_years)))
-  age_dat      = np.zeros((nsims,   int(run_years)  ,len(AGE_HIST_BINS)-1))
-  mort_dat     = np.zeros((nsims,   int(run_years)))
-  pyr_mat      = np.zeros((nsims,   int(run_years)+1,20))-1
+        with open(os.path.join(tpath, P_FILE)) as fid01:
+            param_dict = json.load(fid01)
 
-  mcv1_vec     = np.array(param_dict['EXP_VARIABLE']['MCV1'])
-  mcv1_lvl     = np.unique(mcv1_vec)
+        nsims = int(param_dict[NUM_SIMS])
 
-  mcv1_age_vec = np.array(param_dict['EXP_VARIABLE']['MCV1_age'])
-  mcv1_age_lvl = np.unique(mcv1_age_vec)
+        inf_dat = np.zeros((nsims, 12*int(run_years)))
+        age_dat = np.zeros((nsims, int(run_years), len(AGE_HIST_BINS)-1))
+        mort_dat = np.zeros((nsims, int(run_years)))
+        pyr_mat = np.zeros((nsims, int(run_years)+1, 20))-1
 
-  mcv2_vec     = np.ones(nsims)*param_dict['EXP_CONSTANT']['MCV2']
-  mcv2_lvl     = np.unique(mcv2_vec)
+        mcv1_vec = np.array(param_dict[EXP_V]['MCV1'])
 
-  xval         = np.arange(0,run_years,1/12) + 1/24
-  xyrs         = np.arange(0,run_years,1)    + 1/2
-  xages        = AGE_HIST_BINS[1:] + np.diff(AGE_HIST_BINS)/2
-  mort_prob    = np.interp(xages, IHME_MORT_X, IHME_MORT_Y)
+        mcv1_age_vec = np.array(param_dict[EXP_V]['MCV1_age'])
+        mcv1_age_lvl = np.unique(mcv1_age_vec).tolist()
 
-  for sim_idx_str in data_brick:
-    if(not sim_idx_str.isdigit()):
-      continue
+        mcv2_vec = np.ones(nsims)*param_dict[EXP_C]['MCV2']
+        mcv2_lvl = np.unique(mcv2_vec)
 
-    sim_idx = int(sim_idx_str)
-    inf_dat[sim_idx,:]   = np.array(data_brick[sim_idx_str]['timeseries'])
-    age_dat[sim_idx,:,:] = np.array(data_brick[sim_idx_str]['age_data'])
-    pyr_mat[sim_idx,:,:] = np.array(data_brick[sim_idx_str]['pyr_data'])
-    mort_dat[sim_idx,:]  = np.sum(age_dat[sim_idx,:,:]*mort_prob, axis=1)
+        xval = np.arange(0, run_years, 1/12) + 1/24
+        xyrs = np.arange(0, run_years, 1) + 1/2
+        xages = AGE_HIST_BINS[1:] + np.diff(AGE_HIST_BINS)/2
+        mort_prob = np.interp(xages, IHME_MORT_X, IHME_MORT_Y)
 
+        for skey in data_brick:
+            if (not skey.isdigit()):
+                continue
 
-  fidx = (pyr_mat[:,0,0]>=0)
+            sidx = int(skey)
+            inf_dat[sidx, :] = np.array(data_brick[skey]['timeseries'])
+            age_dat[sidx, :, :] = np.array(data_brick[skey]['age_data'])
+            pyr_mat[sidx, :, :] = np.array(data_brick[skey][POP_PYR])
+            mort_dat[sidx, :] = np.sum(age_dat[sidx, :, :]*mort_prob, axis=1)
 
-  pyr_mat_avg = np.mean(pyr_mat[fidx,:,:],axis=0)
-  tpop_avg    = np.sum(pyr_mat_avg, axis=1)
-  tpop_xval   = np.arange(len(tpop_avg))
-  pops        = np.interp(xyrs, tpop_xval, tpop_avg)
-  mort_nrm    = mort_dat[fidx,:]/pops*1e5
+        fidx = (pyr_mat[:, 0, 0] >= 0)
 
+        pyr_mat_avg = np.mean(pyr_mat[fidx, :, :], axis=0)
+        tpop_avg = np.sum(pyr_mat_avg, axis=1)
+        tpop_xval = np.arange(len(tpop_avg))
+        pops = np.interp(xyrs, tpop_xval, tpop_avg)
+        mort_nrm = mort_dat[fidx, :]/pops*1e5
 
-  # Figures
-  fig01 = plt.figure(figsize=(8,6))
+        # Figures
+        fig01 = plt.figure(figsize=(8, 6))
 
-  # Figures - Sims - Infections
-  axs01 = fig01.add_subplot(1, 1, 1)
-  plt.sca(axs01)
+        # Figures - Sims - Infections
+        axs01 = fig01.add_subplot(1, 1, 1)
+        plt.sca(axs01)
 
-  axs01.grid(visible=True, which='major', ls='-', lw=0.5, label='')
-  axs01.grid(visible=True, which='minor', ls=':', lw=0.1)
-  axs01.set_axisbelow(True)
+        axs01.grid(visible=True, which='major', ls='-', lw=0.5, label='')
+        axs01.grid(visible=True, which='minor', ls=':', lw=0.1)
+        axs01.set_axisbelow(True)
 
-  k1 = 0
-  for mcv1_age_val in mcv1_age_lvl:
-    if(mcv1_age_val > 300):
-      continue
+        for mcv1_age_val in mcv1_age_lvl:
+            if (mcv1_age_val > 300):
+                continue
 
-    idx02 = (mcv1_age_vec == mcv1_age_val)
-    tidx  = (fidx & idx02)
-    xval  = mcv1_vec[tidx]
-    xval2 = np.arange(0.2,1.001,0.01)
-    yval  = np.mean(mort_nrm[tidx,-10:],axis=1)/12.0
-    pcoef = np.polyfit(xval,yval,4)
-    yval2 = np.polyval(pcoef,xval2)
+            idx02 = (mcv1_age_vec == mcv1_age_val)
+            tidx = (fidx & idx02)
+            xval = mcv1_vec[tidx]
+            xval2 = np.arange(0.2, 1.001, 0.01)
+            yval = np.mean(mort_nrm[tidx, -10:], axis=1)/12.0
+            pcoef = np.polyfit(xval, yval, 4)
+            yval2 = np.polyval(pcoef, xval2)
+            lidx = mcv1_age_lvl.index(mcv1_age_val)
+            cval = 'C{:d}'.format(lidx)
 
-    #axs01.scatter(xval, yval, c=yval, vmin=20, vmax=180)
-    axs01.plot(xval,  yval,  '.',  alpha=0.1, color='C{:d}'.format(k1),
-                                   markeredgecolor=None)
-    axs01.plot(xval2, yval2, '-', alpha=1.0, color='C{:d}'.format(k1))
+            axs01.plot(xval, yval, '.', alpha=0.1, color=cval,
+                       markeredgecolor=None)
+            axs01.plot(xval2, yval2, '-', alpha=1.0, color=cval)
 
-    txt_age = int(np.round(mcv1_age_val/365*12))
-    axs01.text( 0.56, 3.76-0.24*k1, 'MCV1 {:>2d}mo'.format(txt_age), fontsize=18,
-                                color='C{:d}'.format(k1))
-    if(mcv2_lvl[0] > 0):
-      axs01.text( 0.74, 3.76-0.24*k1, '+ MCV2 15mo', fontsize=18,
-                                  color='C{:d}'.format(k1))
+            txt_age = int(np.round(mcv1_age_val/365*12))
+            axs01.text(0.56, 3.76-0.24*lidx, 'MCV1 {:>2d}mo'.format(txt_age),
+                       fontsize=18, color=cval)
+            if (mcv2_lvl[0] > 0):
+                axs01.text(0.74, 3.76-0.24*lidx, '+ MCV2 15mo', fontsize=18,
+                           color=cval)
 
-    k1 = k1 + 1
+        axs01.set_ylabel('Monthly Mortality per-100k', fontsize=16)
+        axs01.set_xlabel('MCV Coverage', fontsize=16)
+        axs01.set_ylim(0, 4.0)
+        axs01.set_xlim(0.2, 1.0)
 
+        axs01.tick_params(axis='both', which='major', labelsize=14)
 
-  if(ref_year == 2020):
-    demogname = 'Demog: Early'
-  elif(ref_year == 2040):
-    demogname = 'Demog: Mid'
-  elif(ref_year == 2060):
-    demogname = 'Demog: Late'
+        plt.tight_layout()
+        plt.savefig('fig_trends_{:s}_01.png'.format(dirname))
+        plt.close()
 
-  axs01.set_ylabel('Monthly Mortality per-100k',fontsize=16)
-  axs01.set_xlabel('MCV Coverage',fontsize=16)
-  axs01.set_ylim(  0, 4.0)
-  axs01.set_xlim(0.2, 1.0)
-  #axs01.text( 0.71, 270, demogname, fontsize=18)
-  axs01.tick_params(axis='both', which='major', labelsize=14)
+    return None
 
-  plt.tight_layout()
-  plt.savefig('fig_trends_{:s}_01.png'.format(dirname))
-  plt.close()
+# *****************************************************************************
 
 
-#*******************************************************************************
+if (__name__ == "__main__"):
+
+    make_fig()
+
+# *****************************************************************************
