e18f965859ed664ba787d27f283d31e67618eb1d
Kurt Frey <kfrey@idmod.org>
2024-08-18 21:08:42 -0700

    Sims

---
+++ model_rubella01/experiment_DRC_popEQL_RI/make01_param_dict.py
@@ -0,0 +1,101 @@
+# *****************************************************************************
+#
+# *****************************************************************************
+
+import json
+import os
+import sys
+
+import numpy as np
+
+# Ought to go in emodpy
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'local_python')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'Assets', 'python')))
+from py_assets_common.emod_constants import EXP_C, EXP_V, EXP_NAME, NUM_SIMS, \
+                                            P_FILE
+from ref_dat import R0_xval, R0_ycdf, RI_2013, pop_2019, CBR_val
+
+# *****************************************************************************
+
+# This script makes a json dictionary that is used by the pre-processing script
+# in EMOD. Variable names defined here will be available to use in creating
+# the input files.
+
+# The pre-process script will open the json dict created by this method. For
+# everything with the EXP_V key, that script will assume a list and get a value
+# from that list based on the sim index. For everything with the EXP_C key, it
+# will assume a single value and copy that value.
+
+
+def write_param_dict():
+
+    # Setup
+    param_dict = dict()
+
+    param_dict[EXP_NAME] = 'Rubella01-DRC-Demog:EQL-RI'
+    param_dict[NUM_SIMS] = 13000
+    param_dict[EXP_V] = dict()
+    param_dict[EXP_C] = dict()
+
+    # Random number consistency
+    np.random.seed(4)
+
+    # Convenience naming
+    NSIMS = param_dict[NUM_SIMS]
+    P_VAR = param_dict[EXP_V]
+    P_CON = param_dict[EXP_C]
+
+    # Run number (EMOD random seed)
+    P_VAR['run_number'] = list(range(NSIMS))
+
+    # Label
+    P_VAR['adm01'] = np.random.choice(list(RI_2013.keys()),
+                                      size=NSIMS).tolist()
+
+    # Infectivity
+    P_VAR['R0'] = [np.interp(np.random.rand(), R0_ycdf[adm01], R0_xval)
+                   for adm01 in P_VAR['adm01']]
+
+    # RI rate for MR
+    P_VAR['RI_rate'] = [RI_2013[adm01] for adm01 in P_VAR['adm01']]
+
+    # Crude birth rate target
+    P_VAR['CBR_val'] = [CBR_val[adm01] for adm01 in P_VAR['adm01']]
+
+    # Context
+    P_CON['demog_set'] = 'COD'
+    P_CON['num_nodes'] = 1
+
+    # RI rate multiplier for MR
+    P_CON['RI_rate_mult_yvals'] = [1.0, 1.0]
+    P_CON['RI_rate_mult_xvals'] = [2025.0, 2060.0]
+
+    # Log10 of multiplier on exogeneous case importation
+    P_VAR['log10_import_mult'] = [np.log10(pop_2019[adm01]/1.0e6)
+                                  for adm01 in P_VAR['adm01']]
+
+    # Use constant vital dynamics
+    P_CON['steady_state_demog'] = True
+
+    # Use SIAs
+    P_CON['add_campaigns'] = False
+    P_CON['SIA_coverage'] = 0.6
+    P_CON['SIA_followups'] = False
+
+    # Initial number of agents
+    P_CON['num_agents'] = 100000
+
+    # Write parameter dictionary
+    with open(P_FILE, 'w') as fid01:
+        json.dump(param_dict, fid01)
+
+    return None
+
+# *****************************************************************************
+
+
+if (__name__ == "__main__"):
+
+    write_param_dict()
+
+# *****************************************************************************
---
+++ model_rubella01/experiment_DRC_popEQL_RI_avg/make_burden_csv.py
@@ -0,0 +1,98 @@
+# *****************************************************************************
+
+import json
+import os
+import sys
+
+import numpy as np
+
+# Ought to go in emodpy
+sys.path.append(os.path.abspath(os.path.join('..', '..', 'local_python')))
+sys.path.append(os.path.abspath(os.path.join('..', 'Assets', 'python')))
+from py_assets_common.emod_local_proc import crs_proc
+from py_assets_common.emod_constants import EXP_C, EXP_V, CBR_VEC, \
+                                            NUM_SIMS, P_FILE, POP_PYR
+from global_data import run_years, start_year
+from ref_dat import pop_2019
+
+# *****************************************************************************
+
+
+def make_dat():
+
+    with open('data_brick.json') as fid01:
+        data_brick = json.load(fid01)
+
+    with open(P_FILE) as fid01:
+        param_dict = json.load(fid01)
+
+    nsims = int(param_dict[NUM_SIMS])
+    ss_demog = param_dict[EXP_C]['steady_state_demog']
+    demog_set = param_dict[EXP_C]['demog_set']
+    #ri_vec = np.array(param_dict[EXP_V]['RI_rate'])
+    adm01 = [param_dict[EXP_C]['adm01']]
+
+    #ri_lev = sorted(list(set(ri_vec.tolist())))
+    prov_list = sorted(list(set(adm01)))
+
+    pyr_mat = np.zeros((nsims, int(run_years)+1, 20))-1
+    inf_mat = np.zeros((nsims, int(run_years), 20))
+    birth_mat = np.zeros((nsims, int(run_years)))
+
+    for sim_idx_str in data_brick:
+        sim_idx = int(sim_idx_str)
+        sim_data = data_brick[sim_idx_str]
+        pyr_mat[sim_idx, :, :] = np.array(sim_data[POP_PYR])
+        inf_mat[sim_idx, :, :] = np.array(sim_data['inf_data'])
+        birth_mat[sim_idx, :] = np.array(sim_data[CBR_VEC])
+
+    # Index for simulations with output
+    fidx = (pyr_mat[:, 0, 0] >= 0)
+
+    for prov in prov_list:
+        gidx = (np.array(adm01) == prov) & fidx
+        print(prov)
+
+        # Average population
+        pyr_mat_avg = np.mean(pyr_mat[gidx, :, :], axis=0)
+        pop_tot = np.sum(pyr_mat_avg, axis=1)
+        pop_tot = np.diff(pop_tot)/2.0 + pop_tot[:-1]
+
+        # CRS calculations
+        XDAT = np.arange(start_year, start_year+run_years) + 0.5
+        fname = 'fert_dat_{:s}.csv'.format(demog_set)
+        fnabs = os.path.abspath(os.path.join('..', 'Assets', 'data', fname))
+        (frt_brth, crs_prob_vec) = crs_proc(fnabs, XDAT, pyr_mat_avg, ss_demog)
+
+        # Normalize timeseries required for CRS calculation
+        brth_vec = np.mean(birth_mat[gidx, :], axis=0)
+        norm_crs_timevec = brth_vec/frt_brth
+
+        pop_scale = np.sum([pop_2019[val] for val in pop_2019])/pop_tot[19]
+
+        inf_mat_avg = np.mean(inf_mat[gidx, :, :], axis=0)
+        crs_mat = np.sum(inf_mat[gidx, :, :]*crs_prob_vec[:,0], axis=2)
+
+        crs_out = crs_mat*norm_crs_timevec*pop_scale
+        crs_out = np.cumsum(crs_out[:, -35:-5], axis=1)
+        crs_sort = np.sort(crs_out, axis=1)
+        tidx = int(crs_sort.shape[0]/20)
+        #print(prov, ',', int(np.mean(crs_sort[:,-1])))
+        print(int(np.mean(crs_sort[:,-1])))
+
+        crs_rat = inf_mat_avg*np.transpose(crs_prob_vec)
+        #ydat = np.sum(crs_rat, axis=1)/brth_vec*norm_crs_timevec*1e3
+        yydat = np.sum(crs_rat, axis=1)*norm_crs_timevec*pop_scale
+        yydat = yydat[-35:-5]
+        yydat = np.cumsum(yydat)
+
+    return None
+
+# *****************************************************************************
+
+
+if (__name__ == "__main__"):
+
+    make_dat()
+
+# *****************************************************************************
