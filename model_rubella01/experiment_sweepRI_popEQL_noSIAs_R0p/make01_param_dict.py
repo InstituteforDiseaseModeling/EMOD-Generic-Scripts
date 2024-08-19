f83352cb5498c922c99732d3aa186931e0169807
Kurt Frey <kfrey@idmod.org>
2024-08-18 21:09:51 -0700

    Update experiment names

--- model_rubella01/experiment_sweepRI_popEQL_noSIAs_R0p/make01_param_dict.py
+++ model_rubella01/experiment_sweepRI_popEQL_noSIAs_R0p/make01_param_dict.py
@@ -30,7 +30,7 @@ def write_param_dict():
     # Setup
     param_dict = dict()
 
-    param_dict[EXP_NAME] = 'Rubella01-DemogSteadyState'
+    param_dict[EXP_NAME] = 'Rubella01-SSA-Demog:EQL-R0:Hi'
     param_dict[NUM_SIMS] = 3000
     param_dict[EXP_V] = dict()
     param_dict[EXP_C] = dict()
