6f2e7fdd3bc7ed42647ac4b425bd1b78146ad7b7
Kurt Frey <kfrey@idmod.org>
2024-10-16 15:48:21 -0700

    Linting

--- refdat_poppyr/make_poppyr_inputs.py
+++ refdat_poppyr/make_poppyr_inputs.py
@@ -1,55 +1,59 @@
-#*******************************************************************************
+# *****************************************************************************
 
-import os, sys
+import os
+import sys
 
-import numpy               as np
+import numpy as np
 
-sys.path.insert(0, os.path.join('..','refdat_namesets'))
-from  aux_namematch  import  reprule, tlc_dict
+sys.path.insert(0, os.path.join('..', 'refdat_namesets'))
+from aux_namematch import reprule, tlc_dict
 
-#*******************************************************************************
-
-
-# Set three letter country code
-COUNTRY = ''
+# *****************************************************************************
 
 
-# Parse CSVs
-wpp_file1    = r'WPP2022_POP_F02_1_POPULATION_BY_AGE_BOTH_SEXES_ESTIMATES.csv'
-with open(wpp_file1, errors='ignore') as fid01:
-  flines_rev = [val.strip().split(',') for val in fid01.readlines()]
+def make_pop_dat(TLC=''):
 
-wpp_file2    = r'WPP2022_POP_F02_1_POPULATION_BY_AGE_BOTH_SEXES_MEDIUM_VARIANT.csv'
-with open(wpp_file2, errors='ignore') as fid01:
-  flines_fwd = [val.strip().split(',') for val in fid01.readlines()]
+    # Parse CSVs
+    wppf1 = 'WPP2022_POP_F02_1_POPULATION_BY_AGE_BOTH_SEXES_ESTIMATES.csv'
+    with open(wppf1, errors='ignore') as fid01:
+        flines_rev = [val.strip().split(',') for val in fid01.readlines()]
+
+    wppf2 = 'WPP2022_POP_F02_1_POPULATION_BY_AGE_BOTH_SEXES_MEDIUM_VARIANT.csv'
+    with open(wppf2, errors='ignore') as fid01:
+        flines_fwd = [val.strip().split(',') for val in fid01.readlines()]
+
+    # Construct output data structure
+    pop_dat = np.zeros((0, 22), dtype=int)
+
+    # Add values from retrospective estimates
+    for row_val in flines_rev:
+        if (reprule(row_val[2]) == tlc_dict[TLC]):
+            year_val = int(row_val[10])
+            if (year_val % 5):
+                continue
+            bin_pops = [int(1000*float(row_val[idx].replace(' ', ''))) for idx in range(11, 32)]
+            pop_dat = np.vstack((pop_dat, np.array([year_val]+bin_pops)))
+
+    # Add values from forward projections
+    for row_val in flines_fwd:
+        if (reprule(row_val[2]) == tlc_dict[TLC]):
+            year_val = int(row_val[10])
+            if (year_val % 5):
+                continue
+            if (year_val == pop_dat[-1, 0]):
+                continue
+            bin_pops = [int(1000*float(row_val[idx].replace(' ', ''))) for idx in range(11, 32)]
+            pop_dat = np.vstack((pop_dat, np.array([year_val]+bin_pops)))
+
+    # Write data files
+    ofile_name = 'pop_dat_{:s}.csv'.format(TLC)
+    np.savetxt(ofile_name, pop_dat.T, fmt='%d', delimiter=',')
+
+# *****************************************************************************
 
 
-# Construct output data structure
-pop_dat = np.zeros((0,22), dtype=int)
+if (__name__ == "__main__"):
 
+    make_pop_dat('COD')
 
-# Add values from retrospective estimates
-for row_val in flines_rev:
-  if(reprule(row_val[2]) == tlc_dict[COUNTRY]):
-    year_val = int(row_val[10])
-    if(year_val%5):
-      continue
-    bin_pops = [int(1000*float(row_val[idx].replace(' ',''))) for idx in range(11,32)]
-    pop_dat  = np.vstack((pop_dat,np.array([year_val]+bin_pops)))
-
-# Add values from forward projections
-for row_val in flines_fwd:
-  if(reprule(row_val[2]) == tlc_dict[COUNTRY]):
-    year_val = int(row_val[10])
-    if(year_val%5):
-      continue
-    if(year_val == pop_dat[-1,0]):
-      continue
-    bin_pops = [int(1000*float(row_val[idx].replace(' ',''))) for idx in range(11,32)]
-    pop_dat  = np.vstack((pop_dat,np.array([year_val]+bin_pops)))
-
-
-# Write data files
-np.savetxt('pop_dat_{:s}.csv'.format(COUNTRY), pop_dat.T, fmt='%d', delimiter=',')
-
-#*******************************************************************************
+# *****************************************************************************
